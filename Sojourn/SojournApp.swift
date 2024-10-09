c71ffe68cc6801b9abd43f0ba69a6d05d3caccdf
JeisunDev <56722713+Jason-Karcheski@users.noreply.github.com>
2024-10-09 22:06:17 +0100

    Initial Commit

---
+++ Sojourn/SojournApp.swift
@@ -0,0 +1,17 @@
+//
+//  SojournApp.swift
+//  Sojourn
+//
+//  Created by Jason Karcheski on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SojournApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
